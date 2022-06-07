f0d13f3f460e68fcda5704c68b88794c778dbbce
antieatingactivist <antieatingactivist@gmail.com>
2022-06-07 11:59:53 -0700

    made webpack auto-recompile

--- src/index.js
+++ src/index.js
@@ -3,54 +3,63 @@ import Sprite from "./sprites/Sprite";
 const width = 1000;
 const height = 480;
 
-function main() {
-    gameArea.start();
-    // const canvas = document.querySelector("#glCanvas");
-    // const ctx = canvas.getContext("2d");
-    const sprite = new Sprite(gameArea.context, 20, height);
-      
-    // Only continue if WebGL is available and working
-    // if (ctx === null) {
-    //     alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-    //     return;
-    // }
 
+
+
+
+function main() {
+    var gameArea = {
+        canvas : document.createElement("canvas"),
+        start : function() {
+            this.canvas.width = 1000;
+            this.canvas.height = 500;
+            this.context = this.canvas.getContext("2d");
+            document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+            this.interval = setInterval(updateGameArea, 20);
+        },
+        clear : function() {
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        }
+    }
+    
+    gameArea.start();
+    const sprite = new Sprite(gameArea.context, 20, height);
+    
+    function updateGameArea() {
+        gameArea.clear();
+        sprite.update();
+    }
+    document.addEventListener('keydown', typeLetter);
+        
+    function typeLetter(e) {
+
+        if (e.key == "ArrowLeft" && !e.repeat) {
+            // console.log("ArrowLeft");
+            sprite.movingLeft = true;
+        }
+        if (e.key == "ArrowRight" && !e.repeat) {
+            // console.log("ArrowRight");
+            sprite.movingRight = true;
+        }
+
+    }
+
+    document.addEventListener('keyup', releaseLetter);
+    function releaseLetter(e) {
+        if (e.key == "ArrowLeft") {
+            // console.log("ArrowLeftxx");
+            sprite.movingLeft = false;
+        }
+        if (e.key == "ArrowRight") {
+            // console.log("ArrowRightxx");
+            sprite.movingRight = false;
+        }
+    }
     sprite.init();
 
-    // setInterval(() => {
-    //     gameArea.clear();
-    //     sprite.moveLeft();
-
-    // },100);
-    document.addEventListener('keydown', typeLetter);
     
-    function typeLetter(e) {
-        if (e.key == "ArrowLeft") {   
-            gameArea.clear();
-            sprite.moveLeft();          
-        }
-        if (e.key == "ArrowRight") {   
-            gameArea.clear();
-            sprite.moveRight();          
-        }
-
-    }
+    
 }
-
-var gameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 1000;
-        this.canvas.height = 500;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
-
-
 
 
 window.onload = main;
--- src/sprites/Sprite.js
+++ src/sprites/Sprite.js
@@ -5,24 +5,41 @@ export default class Sprite {
         this.width = 50;
         this.x = x;
         this.y = y-this.height;
+        this.movingLeft = false;
+        this.movingRight = false;
+        this.velocity = 0;
     }
     init() {
         console.log("new sprite");
         this.ctx.fillStyle = "#FF0000";
         this.ctx.fillRect(this.x,this.y,this.width,this.height);
+        
+        
     }
     update() {
+        if(this.movingLeft) {
+            this.moveLeft();
+            this.velocity++;
+        
+        }
+        else if(this.movingRight) {
+            this.moveRight();
+            this.velocity++;
+         
+        }
+        else if (this.velocity > 1) {
+            this.velocity--;
+        }
         this.ctx.fillStyle = "#FF0000";
         this.ctx.fillRect(this.x,this.y,this.width,this.height);
-        // console.log(this.x);
+        
     }
     moveLeft() {
-        this.x--;
-        this.update();
+        console.log(this.velocity);
+        this.x-=this.velocity;
     }
     moveRight() {
-        this.x++;
-        this.update();
+        this.x+=this.velocity;
     }
 
 }
