d3335edba33dca75d34b62ceb26a6b181a67bfff
antieatingactivist <antieatingactivist@gmail.com>
2022-06-17 13:29:54 -0700

    moved player specific methods to player class

--- src/sprites/MovableSprite.js
+++ src/sprites/MovableSprite.js
@@ -20,84 +20,43 @@ export default class MovableSprite extends Sprite {
     } 
     update(xScrollOffset) {
         this.xScrollOffset = xScrollOffset;
-        // this.ctx.fillText(this.xRightVelocity, 840, 100);
-        // this.ctx.fillText(this.standing, 860, 100);
-        // this.ctx.fillText(this.shortJumping, 930, 100);
-        // this.ctx.fillText(this.longJumping, 930, 130);
-        // console.log(this.checkObstacleCeilings());
+
         if (this.isDead) {
             this.y+=10;
             this.color = "#555555";
             this.movingRight = false;
             this.movingLeft = false;
         }
-    //jumping
-        //check for obstacles above while adding to velocity
-        for (var i = 0; i < this.yUpVelocity; i++) {
-            this.y--;
-            if (this.checkObstacleCeilings()) {
-                this.yUpVelocity = 0;
-                this.longJumping = false;
-                this.shortJumping = false;      
-            } 
-        }
-            
-        if (this.yUpVelocity > 0) this.yUpVelocity--;
 
-        if (this.shortJumping && this.standing && !this.isDead) {
-            this.shortJump();   
+        //falling
+        else if ( !this.checkObstacleSurfaces() ) {
+            this.standing = false;
+            this.moveDown();
         }
-        else if (this.longJumping && this.shortJumping) {
-            this.fullJump();
+        else if (this.yUpVelocity == 0) { 
+            this.yDownVelocity = 0;
+            this.standing = true;
         }
-    //falling
-        else  {
-
-            if ( !this.checkObstacleSurfaces() ) {
-                this.standing = false;
-                this.moveDown();
-            }
-            else if (this.yUpVelocity == 0) { 
-                this.yDownVelocity = 0;
-                this.standing = true;
-            }
-        }
-    //moving left
-        if (this.movingLeft && (!this.crouching || this.yUpVelocity > 0)) {
+        
+        //moving left
+        if (this.movingLeft) {
             this.moveLeft();
-            if (this.xLeftVelocity < 20) this.xLeftVelocity+=1;
-
         }
-        else if (this.xLeftVelocity > 0) {
-            this.moveLeft();
-            this.xLeftVelocity--;
+        //moving right
+        if (this.movingRight) {
+            this.moveRight();    
         }
-        else this.xLeftVelocity = 0;
-    //moving right
-        if (this.movingRight && (!this.crouching || this.yUpVelocity > 0)) {
-            this.moveRight();
-            if (this.xRightVelocity < 20) this.xRightVelocity+=1;      
-        }
-        else if (this.xRightVelocity > 0) {
-            this.moveRight();
-            
-            this.xRightVelocity--;
-        }
-        else this.xRightVelocity=0;
-    //crouching
+        //crouching
         if (this.crouching) {
             this.crouch();
         }
         else {
             this.standUp();
         }        
-    //render
-    // this.displayStats();
-
-    return this.render();
-
+        //render
+        this.render();
         
-    }
+    }    
     crouch() {
         this.height = 50; 
     }
--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -12,6 +12,89 @@ export default class Player extends MovableSprite {
         if (this.x > 400) this.offset = -(this.x - 400);
         return this.offset;
     }
+    update(xScrollOffset) {
+        this.xScrollOffset = xScrollOffset;
+        // this.ctx.fillText(this.xRightVelocity, 840, 100);
+        // this.ctx.fillText(this.standing, 860, 100);
+        // this.ctx.fillText(this.shortJumping, 930, 100);
+        // this.ctx.fillText(this.longJumping, 930, 130);
+        // console.log(this.checkObstacleCeilings());
+        if (this.isDead) {
+            this.y+=10;
+            this.color = "#555555";
+            this.movingRight = false;
+            this.movingLeft = false;
+        }
+    //jumping
+        //check for obstacles above while adding to velocity
+        for (var i = 0; i < this.yUpVelocity; i++) {
+            this.y--;
+            if (this.checkObstacleCeilings()) {
+                this.yUpVelocity = 0;
+                this.longJumping = false;
+                this.shortJumping = false;      
+            } 
+        }
+            
+        if (this.yUpVelocity > 0) this.yUpVelocity--;
+
+        if (this.shortJumping && this.standing && !this.isDead) {
+            this.shortJump();   
+        }
+        else if (this.longJumping && this.shortJumping) {
+            this.fullJump();
+        }
+    //falling
+        else  {
+
+            if ( !this.checkObstacleSurfaces() ) {
+                this.standing = false;
+                this.moveDown();
+            }
+            else if (this.yUpVelocity == 0) { 
+                this.yDownVelocity = 0;
+                this.standing = true;
+            }
+        }
+    //moving left
+        if (this.movingLeft && (!this.crouching || this.yUpVelocity > 0)) {
+            this.moveLeft();
+            if (this.xLeftVelocity < 20) this.xLeftVelocity+=1;
+
+        }
+        else if (this.xLeftVelocity > 0) {
+            this.moveLeft();
+            this.xLeftVelocity--;
+        }
+        else this.xLeftVelocity = 0;
+    //moving right
+        if (this.movingRight && (!this.crouching || this.yUpVelocity > 0)) {
+            this.moveRight();
+            if (this.xRightVelocity < 20) this.xRightVelocity+=1;      
+        }
+        else if (this.xRightVelocity > 0) {
+            this.moveRight();
+            
+            this.xRightVelocity--;
+        }
+        else this.xRightVelocity=0;
+    //crouching
+        if (this.crouching) {
+            this.crouch();
+        }
+        else {
+            this.standUp();
+        }        
+    //render
+    // this.displayStats();
+    return this.render();      
+    }
+    crouch() {
+        this.height = 50; 
+    }
+    standUp() {
+        this.height = 100;
+    }
     render() {   
         //render
         this.ctx.fillStyle = this.color;
