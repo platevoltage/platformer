61afb5f7c10b5cfdf20f15ea014bc36973dfbf1f
antieatingactivist <antieatingactivist@gmail.com>
2022-06-19 12:56:00 -0700

    added border to breakable bricks

--- src/sprites/BreakableBrick.js
+++ src/sprites/BreakableBrick.js
@@ -21,7 +21,25 @@ export default class BreakableBrick extends Sprite {
 
         this.render();
     }
-
+    render() {      
+        //render
+        this.ctx.fillStyle = "#111111";
+        this.ctx.fillRect(
+            this.x + this.xScrollOffset,
+            this.y-this.height,
+            this.width,
+            this.height
+        );
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(
+            this.x + this.xScrollOffset+2,
+            this.y-this.height+2,
+            this.width-4,
+            this.height-4
+        );
+        return 0;
+        
+    }
 
 
 
